c4506ff613fc6390034b4fafe319093b9eb3aa13
Max Teradox <max.teradox@hotmail.com>
2013-08-17 15:01:10 +0200

    Revert "Second Commit."
    This reverts commit 67f9ed6488f40b3cce1e33cf85df35c9d4dba316.

--- ShowGit/src/ShowGit_1.java
+++ ShowGit/src/ShowGit_1.java
@@ -4,6 +4,5 @@ public class ShowGit_1 {
     System.out.println("ShowGit_1");
     System.out.println("Hallox!");
     System.out.println("Projekt-Version 1 Fertig.");
-    System.out.println("Schon gehts zur Version 2");
   }
 }
