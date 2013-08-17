5f4028fdefeefbfade1c3a3299c0d45ab02a1263
Max Teradox <max.teradox@hotmail.com>
2013-08-17 14:48:11 +0200

    Initial Commit.

---
+++ ShowGit/src/ShowGit_1.java
@@ -0,0 +1,8 @@
+
+public class ShowGit_1 {
+  public static void main(String[] args) {
+    System.out.println("ShowGit_1");
+    System.out.println("Hallox!");
+    System.out.println("Projekt-Version 1 Fertig.");
+  }
+}
