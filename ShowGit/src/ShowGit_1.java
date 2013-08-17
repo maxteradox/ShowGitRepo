59799a4218bd35fd905e0e1644366b7de0a20b97
Max Teradox <max.teradox@hotmail.com>
2013-08-17 15:05:28 +0200

    Another Commit.

--- ShowGit/src/ShowGit_1.java
+++ ShowGit/src/ShowGit_1.java
@@ -4,5 +4,8 @@ public class ShowGit_1 {
     System.out.println("ShowGit_1");
     System.out.println("Hallox!");
     System.out.println("Projekt-Version 1 Fertig.");
+    System.out.println("Schon gehts zur Version 2");
+    System.out.println("Wurde mal Reverted");
+    System.out.println("Jetzt gehts zur Version 2");
   }
 }
